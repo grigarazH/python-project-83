9374aefd31436586103b789df99aafdef2d9d8df
grigarazh <grigarazh@yandex.ru>
2023-06-20 14:40:39 +0300

    tests: added tests

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,12 +1,7 @@
 from flask import Flask, render_template
 
 
-def create_app():
-    app = Flask(__name__)
-    return create_app
-
-
-app = create_app()
+app = Flask(__name__)
 
 
 @app.route('/')
---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+import pytest
+import os
+from page_analyzer import app
+
+
+def test_index():
+    os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
+    with app.test_client() as client:
+        response = client.get("/")
+        assert '<h1 class="display-3">Анализатор страниц</h1>' in response.text
