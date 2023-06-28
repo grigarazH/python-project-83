dc46730aa5e261f912560d8f89e4daf85ac0358e
grigarazh <grigarazh@yandex.ru>
2023-06-28 16:39:55 +0300

    feat: added url_checks table to db

--- database.sql
+++ database.sql
@@ -1,7 +1,18 @@
+DROP TABLE IF EXISTS url_checks;
 DROP TABLE IF EXISTS urls;
 
 CREATE TABLE urls (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255) UNIQUE NOT NULL,
+    created_at timestamp NOT NULL
+);
+
+CREATE TABLE url_checks(
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id bigint NOT NULL REFERENCES urls(id),
+    status_code int,
+    h1 text,
+    title text,
+    description text,
     created_at timestamp NOT NULL
 );
